c5124b8dcddf51ecb6352a26391f850a0c39f349
ensignavenger <jlv@ensigntech.com>
2012-08-30 18:38:05 -0500

    many improvements
    added filtering to prevent queue from filling up, fixed class name,
    fixed various other mistakes

--- brains/vann.py
+++ brains/vann.py
@@ -32,12 +32,10 @@ Ideas for Improvement:
 2) Some of the logic could be turned into additional functions, making manteniance a bit easier.
 
 '''
-class intelligence(object):
+class Intelligence(object):
     def __init__(self):
         self.world_dimension_x = 10
         self.world_dimension_y = 8
-        self.scan_x = 1
-        self.scan_y = 1
     
     def tile_safe(self,game,direction):
         ''' returns True if tile is safe to enter, returns False if it is not, fires if it contains an obstacle, then returns True
@@ -60,75 +58,90 @@ class intelligence(object):
                 return True
             else:
                 self.face(game,direction)
-                game.shoot()
+                self.shoot(game)
                 return True
         else:
             return False
                 
     def move_x(self,game):
-        if game.tank_positions[0][0]> game.position[0]:
+        if game.tank_positions[0][0] > game.position[0]:
             if self.tile_safe(game,game.RIGHT):
                 self.face(game,game.RIGHT)
                 print('Vann moving Right')
-                game.forward()
-                self.enemy = False
+                self.forward(game)
+                print("Vann's commands are {0}".format(game.memory))
             else:
                 self.face(game,game.RIGHT)
-                game.shoot()
-                self.enemy = False
+                self.shoot(game)
+                print("Vann's commands are {0}".format(game.memory))
 
         else:
-            game.shoot()
-            self.enemy = False
+            self.shoot(game)
+            print("Vann's commands are {0}".format(game.memory))
     
    
     def face(self,game,direction):
-        ''' This function checks to see if tank is already facing direction (UP,DOWN,LEFT,RIGHT) and if not it changes direction, if it is, it does nothing.
+        ''' This function checks to see if tank is already facing direction (UP,DOWN,LEFT,RIGHT) and if not it changes direction, if it is, it does nothing. Also filters to make sure it only has one such command in queue.
         '''
         if game.facing == direction:
             pass
         else:
-            game.face(direction)
-        
+            if direction not in game.memory:
+                game.face(direction)
+            
+    def forward(self,game):
+        ''' This function wraps the game.forward() function in an attempt to prevent the command queue from filling up.  It checks to see if a command already exists to perform an action before adding it again'''
+        if game.FORWARD not in game.memory:
+            game.forward()
+        else:
+            print('Already queued')
+            
+    def shoot(self,game):
+        ''' This function wraps the game.shoot() function in an attempt to prevent the command queue from filling up.  It checks to see if a command already exists to perform an action before adding it again'''
+        if game.SHOOT not in game.memory:
+            game.shoot()
+        else:
+            print('Already queued')
 
     def get_action(self,game):
+        ''' This is where the real logic goes- what does the tank do?'''
+        print("Vann's commands are {0}".format(game.memory))
         if game.tank_positions[0][0] == game.position[0]:
-            if game.tank_positions[0][1] < game.position[1]:
+            if game.tank_positions[0][1] > game.position[1]:
                 self.face(game,game.DOWN)
-                game.shoot()
-                self.enemy = False
+                self.shoot(game)
+                print("Vann's commands are {0}".format(game.memory))
             else:
                 self.face(game,game.UP)
-                game.shoot()
-                self.enemy = False
-                
+                self.shoot(game)
+                print("Vann's commands are {0}".format(game.memory))
+
         elif game.tank_positions[0][1] == game.position[1]:
             if game.tank_positions[0][0] > game.position[0]:
                 self.face(game,game.RIGHT)
-                game.shoot()
-                self.enemy = False
+                self.shoot(game)
+                print("Vann's commands are {0}".format(game.memory))
             else:
                 self.face(game,game.LEFT)
-                game.shoot()
-                self.enemy = False
+                self.shoot(game)
+                print("Vann's commands are {0}".format(game.memory))
         
         else:
-
             if game.tank_positions[0][1] > game.position[1]:
-                if self.tile_safe(game,game.UP):
-                    self.face(game,game.UP)
-                    print('Vann moving up')
-                    game.forward()
-                    self.enemy = False
+                if self.tile_safe(game,game.DOWN):
+                    self.face(game,game.DOWN)
+                    print('Vann moving DOWN')
+                    self.forward(game)
+                    print("Vann's commands are {0}".format(game.memory))
                 else:
                     self.move_x(game)
             
             elif game.tank_positions[0][1] < game.position[1]:
-                if self.tile_safe(game,game.DOWN):
-                    self.face(game,game.DOWN)
-                    print('Vann moving Down')
-                    game.forward()
-                    self.enemy = False
+                if self.tile_safe(game,game.UP):
+                    self.face(game,game.UP)
+                    print('Vann moving UP')
+                    self.forward(game)
+                    print("Vann's commands are {0}".format(game.memory))
                 else:
                     self.move_x(game)
         
@@ -136,7 +149,7 @@ class intelligence(object):
     
 # create an instance of intelligence
 
-my_brain = intelligence()   
+my_brain = Intelligence()   
 def think(game):
     
     my_brain.get_action(game)
